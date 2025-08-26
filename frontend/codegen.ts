e307245cf2ae53e65b0a72ad32c02ff779026eaf
Tanaka Anji <anji.tnk.140114@gmail.com>
2025-08-26 16:49:26 +0900

    code gen

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -1,17 +1,14 @@
-import type { CodegenConfig } from "@graphql-codegen/cli";
+import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   schema: "../api/schema.graphql",
-  documents: ["src/**/*.{ts,tsx}"],
+  documents: [
+    "src/**/*.{ts,tsx}",
+    "!src/gql/**/*",
+  ],
   generates: {
     "src/gql/": {
       preset: "client",
-      // presetConfig: {
-      //   fragmentMasking: false,
-      // },
-      // config: {
-      //   inlineFragmentTypes: "combine",
-      // },
     },
   },
 };
